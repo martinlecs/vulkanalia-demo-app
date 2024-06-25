7b0246d965df2f924f1691f9b231466fb4b9607f
Martin Le <martin.minh.le@gmail.com>
2024-06-25 23:05:54 +1000

    setup vulkan instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::{Entry, Instance};
+use vulkanalia_demo::vulkan::create_vulkan_resources;
 use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
@@ -7,6 +10,8 @@ use winit::window::{Window, WindowId};
 #[derive(Default)]
 struct Application {
     window: Option<Window>,
+    entry: Option<Entry>,
+    instance: Option<Instance>,
 }
 
 impl ApplicationHandler for Application {
@@ -20,12 +25,19 @@ impl ApplicationHandler for Application {
                 )
                 .unwrap(),
         );
+        let (entry, instance) =
+            unsafe { create_vulkan_resources(self.window.as_ref().unwrap()).unwrap() };
+        self.entry = Some(entry);
+        self.instance = Some(instance);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
+                unsafe {
+                    self.instance.as_ref().unwrap().destroy_instance(None);
+                };
             }
             WindowEvent::RedrawRequested => {
                 self.window.as_ref().unwrap().request_redraw();
---
+++ src/vulkan.rs
@@ -0,0 +1,35 @@
+use anyhow::anyhow;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use winit::window::Window;
+
+pub unsafe fn create_vulkan_resources(window: &Window) -> anyhow::Result<(Entry, Instance)> {
+    let loader = LibloadingLoader::new(LIBRARY)?;
+    let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+    let instance = create_instance(window, &entry)?;
+    Ok((entry, instance))
+}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> anyhow::Result<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial (Rust)\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let flags = vk::InstanceCreateFlags::empty();
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    Ok(entry.create_instance(&info, None)?)
+}
